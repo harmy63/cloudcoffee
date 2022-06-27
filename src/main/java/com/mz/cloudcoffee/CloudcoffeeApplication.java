d58214601786c80483c42102f1227299be9f7498
harmy63 <galmang2015@gmail.com>
2022-06-27 13:40:11 +0900

    Add files via upload

---
+++ src/main/java/com/mz/cloudcoffee/CloudcoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.mz.cloudcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mz/cloudcoffee/CloudcoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.cloudcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
